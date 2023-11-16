edc9b3235c8439ada51ad7bc51514c2334b20acf
Dell <Dell@Admin-PC>
2023-11-16 18:33:46 +0700

    du an 1

--- src/Service/PGiaoHangService.java
+++ src/Service/PGiaoHangService.java
@@ -43,6 +43,37 @@ public class PGiaoHangService {
             e.printStackTrace();
         }
         return listGH;
-
     }
+    
+        public ArrayList<PGiaoHang> Search(int id) {
+        this.listGH = new ArrayList<>();
+        try{    
+            String sql = "select id, user_id, address_id, into_money, total_cost, created_at, updated_at, delivery_date, voucher_id, status from bill where id = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setObject(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                 PGiaoHang ph;
+ //               int id = rs.getInt(1);
+                int user = rs.getInt(2);
+                int address_id = rs.getInt(3);
+                double into_money = rs.getDouble(4); 
+                double total_cost = rs.getDouble(5);
+                Date created_at = rs.getDate(6);
+                Date update_at = rs.getDate(7);
+                Date delivery_at = rs.getDate(8);
+                int voucher = rs.getInt(9);
+                String status = rs.getString(10);
+                ph  = new PGiaoHang(id, user, into_money, total_cost, voucher, update_at, update_at, update_at, voucher, status);
+
+            this.listGH.add (ph);
+            }
+            return listGH;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listGH;
+    }
+  
 }
